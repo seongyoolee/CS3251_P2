44d15fca1778b84afd3f8aa2e3277734fb74f017
Seon Gyoo Lee <seongyoolee@Seons-MacBook-Pro.local>
2020-11-18 01:07:22 -0500

    work on server and client features

--- client.py
+++ client.py
@@ -1,18 +1,67 @@
 import socket
 import sys
+import json
 
+BUF_SIZE = 1024
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
+play = True
+letter_attempts = set()
+incorrect_attempts = []
+previous_guess = ""
+previous_server_message = ""
 
 s = socket.socket()
 s.connect((SERVER, PORT))
 
-message = "0"
-s.sendall(message.encode())
+def generate_client_message(data):
+    length = 1 if type(data) == int else len(data)
+    client_message = json.dumps({"msg_length": length, "data": data})
+    return client_message
 
-BUF_SIZE = 1024
 while True:
-    message = s.recv(BUF_SIZE)
-    print(message.decode())
+    user_response = input("Ready to start game? (y/n)").lower()
+    print(user_response)
+    if user_response.isnumeric():
+        word_index = int(user_response) - 1
+        s.sendall(generate_client_message(word_index).encode())
+        break
+    elif user_response == "y":
+        s.sendall(generate_client_message(-1).encode())
+        break
+    elif user_response == "n":
+        s.sendall(generate_client_message(-2).encode())
+        play = False
+        break
+    else:
+        print("Input not valid. Try again.")
+    
+while play:
+    server_message = json.loads(s.recv(BUF_SIZE).decode())
+
+    # print server message right away
+    print(server_message["data"])
+
+    if server_message["msg_flag"] > 0:# check if server sent "message"
+        if server_message["data"] == "Game Over!":
+            break
+    else:# server sent "game progress"
+        if server_message["data"] == previous_server_message:
+            if previous_guess:
+                incorrect_attempts.append(previous_guess.upper())
+        else:
+            previous_server_message = server_message["data"]
+        print("Incorrect Guesses: ", " ".join(incorrect_attempts))
+
+        user_guess = input("\nLetter to guess: ").lower()
+        while user_guess < 'a' or user_guess > 'z' or len(user_guess) != 1 or user_guess in letter_attempts:
+            if user_guess in letter_attempts:
+                print("Error! Letter " + user_guess.upper() + " has been guessed before, please guess another letter.")
+            else:
+                print("Error! Please guess ONE letter.")
+            user_guess = input("\nLetter to guess: ").lower()
+        letter_attempts.add(user_guess)
+        previous_guess = user_guess
+        s.sendall(generate_client_message(user_guess).encode())
 
 s.close()
--- server.py
+++ server.py
@@ -1,14 +1,29 @@
 import socket
 import sys
+import _thread
+import random
+import json
 
-# HOST = sys.argv[0]
+num_connections = 0
+BUF_SIZE = 1024
 HOST = "127.0.0.1"
 PORT = int(sys.argv[1])
 FILE = 'word_dictionary.txt'
+
+# get file
 if len(sys.argv) == 3:
     FILE = sys.argv[2]
 elif len(sys.argv) > 3:
     print("Too many arguments given to run the server.")
+
+# load from word dictionary
+f = open(FILE, "r")
+words = []
+lines = f.readlines()
+for line in lines[1:]:
+    if line[-1] == '\n':
+        line = line[:-1]
+    words.append(line)
 
 # create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +34,7 @@ try:
     print((HOST, PORT))
     s.bind((HOST, PORT))
 except:
-    print("Socket Bind Failure Error: ", socket.error)
+    print("Socket Bind Failure Error: ", socket.error.strerror())
     sys.exit()
 print("Socket Bind Success")
 
@@ -28,12 +43,89 @@ print("Socket Bind Success")
 s.listen()
 print("Socket Listening")
 
-BUF_SIZE = 1024
+def generate_server_message(data, word_to_guess, letter_attempts):
+    if data in ["You Win!", "You Lose :(", "Game Over!"]:
+        return json.dumps({
+            "msg_flag": len(data), 
+            "data": data
+        })
+    else:
+        return json.dumps({
+            "msg_flag": 0, 
+            "word_length": len(word_to_guess), 
+            "num_incorrect": len(letter_attempts - set(word_to_guess)),
+            "data": data
+        })
+
+
+# thread to connect client
+def connect_client(connection, address):
+    print("Get connected from", address[0], ":", address[1])
+
+    word_to_guess = ""
+    word_progress = []
+    letter_attempts = set()
+    num_remaining_attempts = 6
+
+    while True:
+        client_message = connection.recv(BUF_SIZE).decode()
+        client_message = json.loads(client_message)
+        client_data = client_message["data"]
+
+        # print("word to guess: ", word_to_guess)
+
+        if type(client_data) == int:# set word to guess
+            if client_data == -2:
+                break
+            elif client_data == -1:
+                word_to_guess = random.choice(words)
+            else:
+                word_to_guess = words[client_data]
+            word_progress = ["_" for i in range(len(word_to_guess))]
+            progress = " ".join(word_progress)
+            connection.sendall(generate_server_message(progress, word_to_guess, letter_attempts).encode())
+        else:# user/client guesses letters in word
+            if client_data not in letter_attempts and client_data in word_to_guess:
+                letter_attempts.add(client_data)
+
+                # update word progress
+                for i in range(len(word_to_guess)):
+                    if word_to_guess[i] == client_data:
+                        word_progress[i] = client_data
+
+                if "_" not in word_progress:#User Wins
+                    connection.sendall(generate_server_message("You Win!", None, None).encode())
+                    connection.sendall(generate_server_message("Game Over!", None, None).encode())
+                    break
+                else:# keep playing game, send progress
+                    progress = " ".join(word_progress)
+                    connection.sendall(generate_server_message(progress, word_to_guess, letter_attempts).encode())
+            else:
+                num_remaining_attempts -= 1
+                if num_remaining_attempts == 0:
+                    connection.sendall(generate_server_message("You Lose :(", None, None).encode())
+                    connection.sendall(generate_server_message("Game Over!", None, None).encode())
+                    break
+                progress = " ".join(word_progress)
+                connection.sendall(generate_server_message(progress, word_to_guess, letter_attempts).encode())
+
+    print("End the connection from", address[0], ":", address[1])
+    connection.close()
+    global num_connections
+    num_connections -= 1
+    # print("remaining connections: ", num_connections)
+
 # communicate with client
 while True:
     connection, address = s.accept()
-    print("Connected with", address[0], "at PORT", address[1])
 
-    data = connection.recv(BUF_SIZE).decode()
-    connection.sendall("hello world".encode())
-    connection.close()
+    # connect with client
+    if num_connections < 3:
+        num_connections += 1
+        # print("num connections: ", num_connections)
+        _thread.start_new_thread(connect_client, (connection, address, ))
+    else:
+        print("server-overloaded")
+        connection.close()
+
+s.close()
